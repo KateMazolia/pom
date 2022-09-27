b892fc99820abe72086092a13110d3c6e79feb86
Kate Mazolya <kat.fffuuu@gmail.com>
2022-09-28 01:16:22 +0700

    add def: test_search; rename def: test_yandex_search > test_enter_and_clear_the_search_field add comments

--- test_yandex.py
+++ test_yandex.py
@@ -1,11 +1,28 @@
 import pytest
-from Pages.yandex.init import Yandex_searcher
+from Pages.yandex.init import YandexSearcher
+from POM.Pages.yandex.Locators import Locators
 import time
 
-def test_yandex_search():
-    yandex_page = Yandex_searcher()
+# заходит на страницу, набирает в поиске "кот", нажимает найти, выходит из браузера
+def test_enter_and_clear_the_search_field():
+    yandex_page = YandexSearcher()
     yandex_page.go_to_the_site()
-    yandex_page.enter_word("корова")
+    yandex_page.enter_word("кот")
+    time.sleep(1)
     yandex_page.click_on_the_search_button()
     time.sleep(3)
+    yandex_page.close_browser()
+
+
+# заходит на сайт ya.ru, вводит слово "корова", отменяет ввод, вводит слово "кот", нажимает найти и затем выходит из браузера
+def test_search():
+    yandex_page = YandexSearcher()
+    yandex_page.go_to_the_site()
+    yandex_page.enter_word("корова")
+    time.sleep(2)
+    yandex_page.click_on_cancel_input_button()
+    yandex_page.enter_word("кот")
+    time.sleep(1)
+    yandex_page.click_on_the_search_button()
+    time.sleep(2)
     yandex_page.close_browser()
