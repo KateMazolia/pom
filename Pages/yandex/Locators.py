0326656012295c9b23055de3baab5d7959c85615
Kate Mazolya <kat.fffuuu@gmail.com>
2022-09-28 00:49:30 +0700

    add locator cancel_input_button

--- Pages/yandex/Locators.py
+++ Pages/yandex/Locators.py
@@ -6,5 +6,5 @@ from selenium.webdriver.common.by import By
 class Locators:
     search_field = (By.ID, "text")
     search_button = (By.CLASS_NAME, "search3__button mini-suggest__button")
-
+    cancel_input_button = (By.CLASS_NAME, "search3__svg_clear")
 
