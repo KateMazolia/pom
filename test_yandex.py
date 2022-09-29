31f296ecd3e7af6b63a481340e732a7973e00a3a
Kate Mazolya <kat.fffuuu@gmail.com>
2022-09-29 22:47:24 +0700

    add fixtures

--- test_yandex.py
+++ test_yandex.py
@@ -3,21 +3,22 @@ from Pages.yandex.init import YandexSearcher
 from POM.Pages.yandex.Locators import Locators
 import time
 
-# заходит на страницу, набирает в поиске "кот", нажимает найти, выходит из браузера
-def test_enter_and_clear_the_search_field():
-    yandex_page = YandexSearcher()
+@pytest.fixture()
+def yandex_page():
+    return YandexSearcher() #возвращает класс, чтобы можно было к нему обратиться
+
+@pytest.fixture()
+def browser(yandex_page):
     yandex_page.go_to_the_site()
-    yandex_page.enter_word("кот")
-    time.sleep(1)
-    yandex_page.click_on_the_search_button()
-    time.sleep(3)
+    yield
     yandex_page.close_browser()
 
+def test_enter_and_clear_the_search_field():
+    yandex_page.enter_word("кот")
+    yandex_page.click_on_the_search_button()
+    time.sleep(3)
 
-# заходит на сайт ya.ru, вводит слово "корова", отменяет ввод, вводит слово "кот", нажимает найти и затем выходит из браузера
 def test_search():
-    yandex_page = YandexSearcher()
-    yandex_page.go_to_the_site()
     yandex_page.enter_word("корова")
     time.sleep(2)
     yandex_page.click_on_cancel_input_button()
@@ -25,4 +26,30 @@ def test_search():
     time.sleep(1)
     yandex_page.click_on_the_search_button()
     time.sleep(2)
-    yandex_page.close_browser()
+
+
+
+# # если без фикстур это делать:
+# # заходит на страницу, набирает в поиске "кот", нажимает найти, выходит из браузера
+# def test_enter_and_clear_the_search_field():
+#     yandex_page = YandexSearcher()
+#     yandex_page.go_to_the_site()
+#     yandex_page.enter_word("кот")
+#     time.sleep(1)
+#     yandex_page.click_on_the_search_button()
+#     time.sleep(3)
+#     yandex_page.close_browser()
+#
+#
+# # заходит на сайт ya.ru, вводит слово "корова", отменяет ввод, вводит слово "кот", нажимает найти и затем выходит из браузера
+# def test_search():
+#     yandex_page = YandexSearcher()
+#     yandex_page.go_to_the_site()
+#     yandex_page.enter_word("корова")
+#     time.sleep(2)
+#     yandex_page.click_on_cancel_input_button()
+#     yandex_page.enter_word("кот")
+#     time.sleep(1)
+#     yandex_page.click_on_the_search_button()
+#     time.sleep(2)
+#     yandex_page.close_browser()
