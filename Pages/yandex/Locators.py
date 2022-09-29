5df36cb9768f8cb434174a7b6e0387ea4a03c8f0
Kate Mazolya <kat.fffuuu@gmail.com>
2022-09-29 23:24:50 +0700

    add new locator: nav_bar_element

--- Pages/yandex/Locators.py
+++ Pages/yandex/Locators.py
@@ -7,4 +7,5 @@ class Locators:
     search_field = (By.ID, "text")
     search_button = (By.CLASS_NAME, "search3__button.mini-suggest__button")
     cancel_input_button = (By.CLASS_NAME, "search3__svg_clear")
+    nav_bar_element = (By.CLASS_NAME, "service__name")
 
